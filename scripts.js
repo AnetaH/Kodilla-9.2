4fe9521db3ddf7202a935d02b72c1e18ef1cae1e
Aneta Halz <aneta.halz@interia.eu>
2017-03-09 23:33:22 +0100

    After corrections

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-$("span:odd").css('color','red');
+$("span:odd").css('color', 'red');
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-  var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-  $(element).append(button)
+  var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+  $(element).append(button);
 });
 
 $("button").click(function(){
